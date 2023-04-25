bc1052af331bbfc9eac2732d4a9413c4ab7f598c
Augustcode99 <burakodabasi99@yahoo.com>
2023-04-25 19:15:44 +0300

    added state to board

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,23 +1,26 @@
 import "./Board.css";
 import Square from "./Square.jsx";
+import { useState } from "react";
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="Row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="Row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="Row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
