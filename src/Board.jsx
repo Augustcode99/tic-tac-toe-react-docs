5152ed6300bd37915649dbb8c03c7f0eddcf406c
Augustcode99 <burakodabasi99@yahoo.com>
2023-04-25 21:57:52 +0300

    turns and winner declaration added

--- src/Board.jsx
+++ src/Board.jsx
@@ -18,9 +18,41 @@ function Board() {
     setXIsNext(!xIsNext);
     setSquares(newSquares);
   };
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  let status;
+  const winner = calculateWinner(squares);
+
+  if (winner) {
+    status = `The winner is ${winner}`;
+  } else {
+    status = `${xIsNext ? "X" : "O"}'s turn`;
+  }
 
   return (
     <>
+      <div>{status}</div>
       <div className="Row">
         <Square
           value={squares[0]}
@@ -83,29 +115,6 @@ function Board() {
       </div>
     </>
   );
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
 }
 
 export default Board;
